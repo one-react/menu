f36f0e5d4242abd3a90d39fa372efb3047ce43f1
foryuki <foryuki@outlook.com>
2018-11-05 21:11:24 +0800

    fix: image url

--- examples/.storybook/webpack.config.js
+++ examples/.storybook/webpack.config.js
@@ -16,6 +16,10 @@ module.exports = (baseConfig, env, defaultConfig) => {
     ]
   })
 
+  if (env === 'production'.toUpperCase()) {
+    defaultConfig.output.publicPath = '/menu/'
+  }
+
   defaultConfig.resolve.alias['or-menu'] = path.resolve(__dirname, '../../src')
 
   defaultConfig.resolve.extensions.push('.ts', '.tsx')
